7aa792f418785751db464831cd8b6462f5c7639f
Stredox02 <29194230+Stredox02@users.noreply.github.com>
2024-06-24 20:50:21 +0200

    Fixed missing annotation

--- src/main/java/org/teleight/td/api/ApiMethod.java
+++ src/main/java/org/teleight/td/api/ApiMethod.java
@@ -1,11 +1,12 @@
 package org.teleight.td.api;
 
 import it.tdlight.jni.TdApi;
+import org.jetbrains.annotations.NotNull;
 
 public interface ApiMethod<F extends TdApi.Function<O>, O extends TdApi.Object, R extends ApiObject> {
 
-    F wrapRequest();
+    @NotNull F wrapRequest();
 
-    R wrapResponse(O internalResponse);
+    @NotNull R wrapResponse(@NotNull O internalResponse);
 
 }
